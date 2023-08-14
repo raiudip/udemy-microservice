3409b1796f05b115cbcbe27a99114d3c6d724a70
Sanush Shakya <shakyasanush7@gmail.com>
2023-08-14 16:40:55 +0545

    sanush - changes in query

--- tutorial/blog/query/index.js
+++ tutorial/blog/query/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { compile } = require("sass");
 
 const app = express();
 app.use(bodyParser.json());
