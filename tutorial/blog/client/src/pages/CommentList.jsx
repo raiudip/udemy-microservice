f4058dad18caa2a49e1711e7da8d5783e434aba7
udip-rai <udiprai.co@gmail.com>
2023-08-15 11:37:28 +0545

    udip - did comment moderation

--- tutorial/blog/client/src/pages/CommentList.jsx
+++ tutorial/blog/client/src/pages/CommentList.jsx
@@ -1,7 +1,21 @@
 // eslint-disable-next-line react/prop-types
 const CommentList = ({ comments }) => {
   const renderedComments = Object.values(comments)?.map((comment, idx) => {
-    return <li key={`${idx}. ${comment.id}`}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation.";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected.";
+    }
+
+    return <li key={`${idx}. ${comment.id}`}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
