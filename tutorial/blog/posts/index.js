ba38362cb4145da79b95e521ea02041a514c207c
Sanush Shakya <shakyasanush7@gmail.com>
2023-08-15 11:52:52 +0545

    sanush - event handling error fixed

--- tutorial/blog/posts/index.js
+++ tutorial/blog/posts/index.js
@@ -1,42 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
-// View all posts
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-// Create a post
 app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
-  posts[id] = { id, title };
 
-  // Right after we add a new post to the collection
+  posts[id] = {
+    id,
+    title,
+  };
+
   await axios.post("http://localhost:4005/events", {
     type: "PostCreated",
-    data: { id, title },
+    data: {
+      id,
+      title,
+    },
   });
 
   res.status(201).send(posts[id]);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type);
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
 
   res.send({});
-})
+});
 
-// Server
 app.listen(4000, () => {
-  console.log("listening on 4000");
+  console.log("Listening on 4000");
 });
